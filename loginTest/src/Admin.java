bce17d2493e4f955aaf661d6db3f7afbbb39a33b
GoLunar <golunarproject@gmail.com>
2017-10-16 12:08:33 -0400

    Project Start

---
+++ loginTest/src/Admin.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author igold
+ */
+public class Admin{
+      private int id;
+      private String firstName;
+      private String lastName;
+      private int age;
+
+
+	/**
+	* Default empty Admin constructor
+	*/
+	public Admin() {
+		super();
+	}
+
+	/**
+	* Default Admin constructor
+	*/
+	public Admin(int id, String firstName, String lastName, int age) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	/**
+	* Returns value of id
+	* @return
+	*/
+	public int getId() {
+		return id;
+	}
+
+	/**
+	* Sets new value of id
+	* @param
+	*/
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	* Returns value of firstName
+	* @return
+	*/
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	* Sets new value of firstName
+	* @param
+	*/
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	* Returns value of lastName
+	* @return
+	*/
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	* Sets new value of lastName
+	* @param
+	*/
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	* Returns value of age
+	* @return
+	*/
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	* Sets new value of age
+	* @param
+	*/
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
+
