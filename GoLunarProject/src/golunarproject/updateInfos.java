59431f9d3b3314b0598b01dbb05adba283f04401
earamburo <31488734+earamburo@users.noreply.github.com>
2017-12-03 14:11:49 -0500

    Commented out
    Database for Update Infos

--- GoLunarProject/src/golunarproject/updateInfos.java
+++ GoLunarProject/src/golunarproject/updateInfos.java
@@ -153,40 +153,40 @@ class updateInfos{
                                 
             
 
-				try {
-					actiontarget.setFill(Color.FIREBRICK);
-                                           actiontarget.setText("Information Updated");
-                                    
-                                    
-                                        Connection connect = null;
-					connect = sqliteConnection.dbconnector();
-
-					String Signupstring = "Insert into UpdateInfo values(?,?,?,?,?,?,?,?,?)";
-					PreparedStatement prepstate;
-
-					prepstate = connect.prepareStatement(Signupstring);
-
-					prepstate.setString(1, fName.getText());
-					prepstate.setString(2, mName.getText());
-					prepstate.setString(3, lName.getText());
-					prepstate.setString(4, add.getText());
-					prepstate.setString(5, pN.getText());
-                                        prepstate.setString(6, e.getText());
-					prepstate.setString(7, DOB.getText());
-                                        prepstate.setString(8, eCN.getText());
-					prepstate.setString(9, eCPN.getText());
-                                        
-					int result = prepstate.executeUpdate();
-				
-                                        connect.commit();
-					
-					prepstate.close();
-
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					System.out.println(e.toString());
-					e.printStackTrace();
-				}
+//				try {
+//					actiontarget.setFill(Color.FIREBRICK);
+//                                           actiontarget.setText("Information Updated");
+//                                    
+//                                    
+//                                        Connection connect = null;
+//					connect = sqliteConnection.dbconnector();
+//
+//					String Signupstring = "Insert into UpdateInfo values(?,?,?,?,?,?,?,?,?)";
+//					PreparedStatement prepstate;
+//
+//					prepstate = connect.prepareStatement(Signupstring);
+//
+//					prepstate.setString(1, fName.getText());
+//					prepstate.setString(2, mName.getText());
+//					prepstate.setString(3, lName.getText());
+//					prepstate.setString(4, add.getText());
+//					prepstate.setString(5, pN.getText());
+//                                        prepstate.setString(6, e.getText());
+//					prepstate.setString(7, DOB.getText());
+//                                        prepstate.setString(8, eCN.getText());
+//					prepstate.setString(9, eCPN.getText());
+//                                        
+//					int result = prepstate.executeUpdate();
+//				
+//                                        connect.commit();
+//					
+//					prepstate.close();
+//
+//				} catch (SQLException e) {
+//					// TODO Auto-generated catch block
+//					System.out.println(e.toString());
+//					e.printStackTrace();
+//				}
 
 				stage1.close();
 
